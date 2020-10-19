fa23d747e337633a7c1ecb08967bc7caa765576d
Ali Babaoglu <ali.babaoglu@hotmail.de>
2020-10-19 22:49:01 +0200

    Quizlogik V1 // Game.js kommentiert

---
+++ Welt-Quizspiel/data.js
@@ -0,0 +1,53 @@
+var DATA = {
+  "NAM": {
+    "NAM1":
+    {
+      "Frage": "Wie lautet das größte Land der Region?",
+      "Antworten": [
+        "Deutschland",
+        "Frankreich",
+        "Mexico",
+        "Schweden"
+      ],
+      "Lösung": 2
+    },
+    "NAM2":
+    {
+
+      "Frage": "Wie groß ist die Region?",
+      "Antworten": [
+        "130KM²",
+        "120KM²",
+        "100KM²",
+        "200KM²"
+      ],
+      "Lösung": 3
+    }
+  },
+  "CAM": {
+    "CAM1":
+    {
+
+      "Frage": "Wie lautet das größte Land der Region?",
+      "Antworten": [
+        "USA",
+        "Mexico",
+        "Haiti",
+        "Cuba"
+      ],
+      "Lösung": 1
+    },
+    "CAM2":
+    {
+      "Frage": "Wie groß ist die Region?",
+      "Antworten": [
+        "130KM²",
+        "120KM²",
+        "100KM²",
+        "200KM²"
+      ],
+      "Lösung": 4
+    }
+
+  }
+}
---
+++ Welt-Quizspiel/main.js
@@ -0,0 +1,27 @@
+const {app, BrowserWindow} = require('electron')
+const url = require('url')
+const path = require('path')
+
+let win
+
+function createWindow() {
+   win = new BrowserWindow({width: 800, height: 600})
+   win.loadURL(url.format ({
+      pathname: path.join(__dirname, 'html/index.html'),
+      protocol: 'file:',
+      slashes: true
+   }))
+}
+
+app.on('ready', createWindow)
+
+function playAudio() {
+    var x = document.getElementById("menu-audio");
+    x.play();
+
+}
+function pauseAudio() {
+    var x = document.getElementById("menu-audio");
+    x.pause();
+
+}
