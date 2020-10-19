232a7561897277a882c5bb2ca6c90fac3449d971
unknown <ghfb@live.de>
2020-10-19 10:53:40 +0200

    ic

--- Welt-Quizspiel/game.js
+++ Welt-Quizspiel/game.js
@@ -1,3 +1,16 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:c49b58e81b4abe54b17d1f422a622b7daff064b5338adbe54b1cbdf9778209ec
-size 250
+
+function startGame() {
+    myGameArea.start();
+}
+
+
+function playAudio() {
+    var x = document.getElementById("menu-audio");
+    x.play();
+
+}
+function pauseAudio() {
+    var x = document.getElementById("menu-audio");
+    x.pause();
+
+}
