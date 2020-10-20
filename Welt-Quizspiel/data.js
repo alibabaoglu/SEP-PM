a0b32fa70d0c8bad724b4b51f37692c37f788a22
Ali Babaoglu <ali.babaoglu@hotmail.de>
2020-10-20 14:01:08 +0200

    spiel.html merged with branch GUI_Karten

--- Welt-Quizspiel/data.js
+++ Welt-Quizspiel/data.js
@@ -2,52 +2,102 @@ var DATA = {
   "NAM": {
     "NAM1":
     {
-      "Frage": "Wie lautet das größte Land der Region?",
+      "Frage": "Wie lautet das größte Land von Nordamerika?",
       "Antworten": [
-        "Deutschland",
-        "Frankreich",
+        "USA",
+        "Canada",
         "Mexico",
-        "Schweden"
+        "Grönland"
       ],
-      "Lösung": 2
+      "Lösung": 4
     },
     "NAM2":
     {
 
-      "Frage": "Wie groß ist die Region?",
+      "Frage": "Wie groß ist die Nordamerika?",
       "Antworten": [
-        "130KM²",
-        "120KM²",
-        "100KM²",
-        "200KM²"
+        "13000000KM²",
+        "12000000KM²",
+        "10000000KM²",
+        "24710000KM²"
       ],
-      "Lösung": 3
+      "Lösung": 4
     }
   },
   "CAM": {
     "CAM1":
     {
 
-      "Frage": "Wie lautet das größte Land der Region?",
+      "Frage": "Wie lautet das größte Land Zentralamerikas?",
       "Antworten": [
-        "USA",
-        "Mexico",
         "Haiti",
+        "Mexico",
+        "Panama",
+        "Cuba"
+      ],
+      "Lösung": 2
+    },
+    "CAM2":
+    {
+      "Frage": "Wie groß ist Zentralamerika?",
+      "Antworten": [
+        "750000KM²",
+        "120000KM²",
+        "100000KM²",
+        "200000KM²"
+      ],
+      "Lösung": 1
+    }
+
+  },
+  "SAM": {
+    "SAM1":
+    {
+      "Frage": "Wie lautet das größte Land Südamerikas?",
+      "Antworten": [
+        "Brazilien",
+        "Argentinien",
+        "USA",
+        "Canada"
+      ],
+      "Lösung": 1
+    },
+    "SAM2":
+    {
+
+      "Frage": "Wie groß ist Südamerika",
+      "Antworten": [
+        "1300000000KM²",
+        "17.840.000KM²",
+        "1000000000KM²",
+        "200K0000000M²"
+      ],
+      "Lösung": 2
+    }
+  },
+  "NAS": {
+    "NAS1":
+    {
+      "Frage": "Wie lautet das größte Land von Nordasien?",
+      "Antworten": [
+        "Russlan",
+        "China",
+        "Deutschland",
         "Cuba"
       ],
       "Lösung": 1
     },
-    "CAM2":
+    "NAS2":
     {
-      "Frage": "Wie groß ist die Region?",
-      "Antworten": [
-        "130KM²",
-        "120KM²",
-        "100KM²",
-        "200KM²"
-      ],
-      "Lösung": 4
-    }
 
+      "Frage": "Wie groß ist Nordasien?",
+      "Antworten": [
+        "13000000KM²",
+        "12000000KM²",
+        "10000000KM²",
+        "20000000KM²"
+      ],
+      "Lösung": 1
+    }
   }
 }
